5d7bca40c3e320d54af69db46bbf75a42fcab933
Kevin <vincitbussiness@gmail.com>
2024-07-28 23:49:01 -0600

    Maquetando la vista de diario

--- app/diario/page.tsx
+++ app/diario/page.tsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const Diario = () => {
     return (
-        <div>Diario</div>
+        <div className='flex flex-col w-full items-start p-10'>
+            <h1 className="text-4xl font-bold ">Diario</h1>
+            <button className='text-center w-full my-3 bg-dark-green text-white px-10 py-3 rounded-xl hover:bg-light-green hover:animate-in'>Actualizar diario</button>
+        </div>
     )
 }
 
