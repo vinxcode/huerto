7736255726ff9cad06c4e49e49fbdafe2839f6e9
Kevin <vincitbussiness@gmail.com>
2024-07-30 19:57:22 -0600

    Creating links to cultivos

--- app/control/page.tsx
+++ app/control/page.tsx
@@ -2,7 +2,19 @@ import React from 'react'
 
 const Control = () => {
   return (
-    <div>Control</div>
+    <div className='flex flex-col w-full items-start py-10 px-7'>
+      <h1 className="text-4xl font-bold ">Control</h1>
+      <button className='text-center w-full my-3 bg-dark-green text-white px-10 py-3 shadow-xl rounded-xl hover:bg-light-green hover:animate-in'>Agregar cultivo</button>
+      <a className='bg-white rounded-xl w-full px-10 py-3 shadow-xl cursor-pointer mb-2'>
+        <h3 className='text-dark-green text-center font-light'>Tomates</h3>
+      </a>
+      <a className='bg-white rounded-xl w-full px-10 py-3 shadow-xl cursor-pointer mb-2'>
+        <h3 className='text-dark-green text-center font-light'>Tomates</h3>
+      </a>
+      <a className='bg-white rounded-xl w-full px-10 py-3 shadow-xl cursor-pointer mb-2'>
+        <h3 className='text-dark-green text-center font-light'>Tomates</h3>
+      </a>
+    </div>
   )
 }
 
