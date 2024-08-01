6c76b160a6d1bc815c4ce40b775f8d0acfc583e1
Kevin <vincitbussiness@gmail.com>
2024-08-01 11:06:57 -0600

    Repairing the control view. There was an error in the database. I was trying to fix the code, but it was the data

--- app/control/page.tsx
+++ app/control/page.tsx
@@ -1,19 +1,20 @@
+import React from 'react'
 import { createClient } from "@/utils/supabase/server";
 
 const Control = async () => {
 
   const supabase = createClient()
-  const { data: cultivos } = await supabase.from("cultivos").select();
+  const { data: todos } = await supabase.from("cultivos").select();
 
   return (
     <section className='flex flex-col w-full items-start py-10 px-7 animate-in'>
       <h1 className="text-4xl font-bold ">Control</h1>
       <button className='text-center w-full my-3 bg-dark-green text-white px-10 py-3 shadow-xl rounded-xl hover:bg-light-green hover:animate-in'>Agregar cultivo</button>
       {
-        cultivos && (
-          cultivos.map(cultivo => (
+        todos && (
+          todos.map(todo => (
             <a className='bg-white rounded-xl w-full px-10 py-3 shadow-xl cursor-pointer mb-2'>
-              <h3 className='text-dark-green text-center font-light'>{cultivo.nombre_cultivo}</h3>
+              <h3 className='text-dark-green text-center font-light'>{todo.id_cultivo} - {todo.nombre_cultivo}</h3>
             </a>
           ))
         )
