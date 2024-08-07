3466024fefaafaf49415af71316e52a281b9a3e9
Kevin <vincitbussiness@gmail.com>
2024-08-07 00:15:19 -0600

    Componente calendario

---
+++ app/control/[cultivo]/[siembra]/CalendarioCultivo.tsx
@@ -0,0 +1,44 @@
+'use client'
+
+import { useState, useEffect } from 'react'
+import { createClient } from '@/utils/supabase/client'
+
+const CalendarioCultivo = () => {
+
+  const supabase = createClient()
+  const [datos, setDatos] = useState([])
+
+  useEffect(() => {
+    const getTodosSiembra = async () => {
+      const { data, error } = await supabase
+        .from('todos_siembra')
+        .select(`
+        id_todo_siembra,
+        descripcion_pendiente,
+        fecha_todo,
+        is_completed`)
+        .eq('siembra', 1)
+      setDatos(data)
+    }
+    getTodosSiembra()
+  }, [supabase])
+
+  return (
+    <article className='bg-white border-2 border-light-green rounded-xl p-4 mt-5 flex flex-col gap-3'>
+      <h3 className='font-semibold text-dark-green'>Calendario de cultivo</h3>
+      <div className='flex flex-col gap-2'>
+        {
+          datos.map((dato, index) => (
+            <div key={index}>
+              <p className='text-sm'>{dato.descripcion_pendiente}</p>
+              <p className='text-sm font-light'>{dato.fecha_todo}</p>
+            </div>
+          ))
+        }
+      </div>
+      <button className='w-full border-2 border-light-green py-2 rounded-xl text-dark-green hover:bg-light-green hover:text-dark-grey'>Agregar fecha</button>
+    </article>
+  )
+}
+
+export default CalendarioCultivo
--- app/control/[cultivo]/[siembra]/page.tsx
+++ app/control/[cultivo]/[siembra]/page.tsx
@@ -1,15 +1,15 @@
 'use client'
 
-import { useParams, usePathname } from 'next/navigation'
+import { useParams } from 'next/navigation'
 import { useStore } from '@/app/store/useStore'
 import { useEffect, useState } from 'react'
 import { createClient } from '@/utils/supabase/client'
+import CalendarioCultivo from './CalendarioCultivo'
 
 const detalleSiembra = () => {
 
   const supabase = createClient()
 
-  const pathname = usePathname()
   const { siembra } = useParams()
   const [datos, setDatos] = useState([])
 
@@ -34,14 +34,13 @@ const detalleSiembra = () => {
 
         `)
         .eq('id_siembra', siembra)
-      console.log(data)
       setDatos(data)
     }
     getSiembras()
   }, [supabase])
 
   return (
-      <>
+    <>
       {/* <button className='text-center w-11/12 my-3 bg-dark-green text-white px-10 py-3 shadow-xl rounded-xl hover:bg-dark-grey  hover:animate-in'>Agregar siembra</button> */}
 
       {
@@ -50,12 +49,19 @@ const detalleSiembra = () => {
             <header className='bg-dark-green h-[120px] w-full py-10 px-7 rounded-b-3xl'>
               <h1 className='text-4xl text-light-grey font-bold'>{dato.cultivos.nombre_cultivo}</h1>
               <p className='text-xl text-light-grey'>{dato.fecha_siembra}</p>
-              <p></p>
             </header>
+            <div className='flex flex-col p-4 w-full'>
+              <article className='flex flex-col text-dark-grey bg-white rounded-xl shadow-lg p-5 w-full'>
+                <p className='text-sm'>Fecha de puesta de germinacion: <span className='font-semibold text-dark-green'>{dato.fecha_siembra}</span></p>
+                <p className='text-sm'>Semillas a germinar: <span className='font-semibold text-dark-green'>{dato.semillas_a_germinar}</span></p>
+                <p className='text-sm'>Estado de los cultivos: <span className='font-semibold text-dark-green'>{dato.estado_cultivos.nombre_estado_cultivo}</span></p>
+              </article>
+              <CalendarioCultivo/>
+            </div>
           </section>
         ))
       }
-  </>
+    </>
   )
 }
 
