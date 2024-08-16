dd099cf054c56ca3bcbcfb3a571a48e0aed98fcf
Kevin <vincitbussiness@gmail.com>
2024-08-15 23:20:01 -0600

    Adding responsivness

--- app/api/diarios/route.ts
+++ app/api/diarios/route.ts
@@ -6,12 +6,12 @@ const supabase = createClient(supabaseUrl, supabaseKey);
 
 
 export async function POST(request: any) {
-    const { fecha_siembra, cultivo, semillas_a_germinar } = await request.json();
+    const { notas, fecha } = await request.json();
 
     try {
         const { data, error } = await supabase
-            .from('siembras')
-            .insert([{ fecha_siembra, semillas_a_germinar, cultivo }]);
+            .from('diarios')
+            .insert([{ notas, fecha }]);
 
         if (error) {
             throw error;
--- app/diario/CrearDiario.tsx
+++ app/diario/CrearDiario.tsx
@@ -23,7 +23,8 @@ export default function CrearDiario() {
                     'Content-Type': 'application/json'
                 },
                 body: JSON.stringify({
-
+                    notas: notas,
+                    fecha: hoy.toDateString()
                 })
             });
 
@@ -40,7 +41,13 @@ export default function CrearDiario() {
         setNotas('')
         updateIsModalDiarioOpen(false)
         setSemillas(0)
+        setText(false)
     };
+
+    const handleCancelar = () => {
+        updateIsModalDiarioOpen(false)
+        setText(false)
+    }
 
     return (
         <>
@@ -48,39 +55,37 @@ export default function CrearDiario() {
                 isModalDiarioOpen && (
                     <div className=' absolute z-10 flex justify-center items-center w-full h-screen modal'>
                         <div className=' flex flex-col gap-3 bg-bg-light-grey shadow-xl p-7 w-full  md:rounded-xl md:w-[600px] h-screen md:h-auto animate-in'>
-                            <h1 className='text-center font-semibold text-lg'>Actualizar diario</h1>
-                            <p className='text-center mt-[-12px]'>{hoy.toDateString()}</p>
-                            {/* <div className='flex items-center justify-between mb-2 px-5'>
-                                <label className='mb-[-10px]'>Cuaderno</label>
-                                <button className='icon-[ion--edit]'
-                                onClick={() => setIsNotasDisabled(false)}></button>
-                            </div> */}
+                            <div className='h-full flex flex-col gap-3'>
+                                <h1 className='text-center font-semibold text-lg'>Actualizar diario</h1>
+                                <p className='text-center mt-[-12px]'>{hoy.toDateString()}</p>
 
-                            <div className='flex flex-col bg-white px-4  py-3 text-sm rounded-xl'
-                                onClick={() => setText(true)}>
-                                <p className={`${ text ? "cursor-click" : "cursor-text" }`}>Agregar una nota</p>
-                                {
-                                    text && (
-                                        <textarea placeholder='Notas del dia' className='p-3 focus:border-white focus:ring focus:ring-white focus:outline-none bg-white rounded-xl'
-                                            rows={8}
-                                            value={notas}
-                                            onChange={(e) => setNotas(e.target.value)}
-                                        >
-                                        </textarea>
-                                    )
-                                }
+
+                                <div className='flex flex-col bg-white px-4  py-3 text-sm rounded-xl shadow-lg'
+                                    onClick={() => setText(true)}>
+                                    <p className={`${text ? "cursor-click" : "cursor-text"} text-center md:text-left`}>Agregar una nota</p>
+                                    {
+                                        text && (
+                                            <div>
+                                                <textarea placeholder='Notas del dia' className='p-3 mt-2 focus:border-white focus:ring focus:ring-white focus:outline-none bg-white rounded-xl'
+                                                    rows={8}
+                                                    value={notas}
+                                                    onChange={(e) => setNotas(e.target.value)}
+                                                >
+                                                </textarea>
+                                                <div className='flex justify-end px-7'>
+                                                    <button className='flex items-center'><span className="icon-[lets-icons--done] text-lg"></span>Guardar</button>
+                                                </div>
+                                            </div>
+                                        )
+                                    }
+
+                                </div>
+
                             </div>
-                            {/* <textarea placeholder='Notas del dia' className='p-3 bg-bg-light-grey enabled:bg-white rounded-xl'
-                                rows={8} disabled={isNotasDisabled}
-                                value={notas}
-                                onChange={(e) => setNotas(e.target.value)}
-                                >
-                            </textarea>
- */}
 
-                            <div className='flex w-full gap-2 text-sm font-semibold'>
+                            <div className='flex w-full justify-center gap-2 text-sm font-semibold'>
                                 <button className='py-3 border-2 border-dark-green text-dark-green w-2/5 rounded-xl'
-                                    onClick={() => updateIsModalDiarioOpen(false)}
+                                    onClick={handleCancelar}
                                 >Cancelar</button>
                                 <button className='py-3 bg-dark-green text-white w-3/5 rounded-xl'
                                     onClick={handleSubmit}
